ad4929ac884aade8cdc95b583ac94776821e8510
Joseph Gomez <jgomez@tunein.com>
2022-11-17 19:12:29 -0800

    Build demo

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        include: /src/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                auto: true,
+              },
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  postcssPresetEnv({
+                    preserve: false,
+                    stage: 2,
+                    features: {
+                      'custom-media-queries': true,
+                    },
+                    importFrom: [
+                      {
+                        customMedia: {
+                          '--min-width-small': `(min-width: 640px)`,
+                          '--max-width-medium': `(min-width: 768px)`,
+                        },
+                      },
+                    ],
+                  }),
+                ],
+              },
+            },
+          },
+          // { loader: path.resolve('./postCssLoaderFix') }, // uncommment to apply fix
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+};
